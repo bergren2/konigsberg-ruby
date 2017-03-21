02de316dae024a6db7036b3022fb7e309dd2ccc9
bergren2 <dan@heymrbass.com>
2017-03-21 16:59:41 -0500

    wrap in objects for sbt support

--- scala/1.scala
+++ scala/1.scala
@@ -1,10 +1,14 @@
-var sum = 0
-val limit = 1000
+object Problem1 {
+  def main(args: Array[String]) {
+    var sum = 0
+    val limit = 1000
 
-// need: modulo, iterate over numbers
-for (i <- 1 to limit - 1) {
-  if (i % 3 == 0) sum += i
-  else if (i % 5 == 0) sum += i
+    // need: modulo, iterate over numbers
+    for (i <- 1 to limit - 1) {
+      if (i % 3 == 0) sum += i
+      else if (i % 5 == 0) sum += i
+    }
+
+    println(sum)
+  }
 }
-
-println(sum)
