91d498ac21564023f9f447a7ed4e4ab1db30d506
bergren2 <dan@heymrbass.com>
2017-03-21 16:42:00 -0500

    add Scala solutions for 1 and 2

---
+++ scala/1.scala
@@ -0,0 +1,10 @@
+var sum = 0
+val limit = 1000
+
+// need: modulo, iterate over numbers
+for (i <- 1 to limit - 1) {
+  if (i % 3 == 0) sum += i
+  else if (i % 5 == 0) sum += i
+}
+
+println(sum)
---
+++ scala/2.scala
@@ -0,0 +1,24 @@
+// n is the nth term in the fib sequence starting with 1, 2
+var fib_cache = Map(1 -> 1, 2 -> 2) // padded with 0 so index matches fib position
+
+def fib (n: Int): Int = n match {
+  case 1 => fib_cache(1)
+  case 2 => fib_cache(2)
+  case _ => {
+    fib_cache.get(n) match {
+      case None => fib_cache += (n -> (fib(n-1) + fib(n-2)))
+      case _ =>
+    }
+    fib_cache(n)
+  }
+}
+
+var sum = 0
+var i = 1
+
+while (fib(i) < 4000000) {
+  if (fib(i) % 2 == 0) sum += fib(i)
+  i += 1
+}
+
+println(sum)
