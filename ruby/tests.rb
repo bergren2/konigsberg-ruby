98559e7eda6c973c7ffa907fd1449b822608adcb
bergren2 <dan@heymrbass.com>
2017-05-12 21:39:16 -0500

    run tests in parallel

--- ruby/tests.rb
+++ ruby/tests.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/parallel"
 require "minitest/pride"
 
 require "./assertions.rb"
@@ -7,6 +8,8 @@ require "./lib/euler_project.rb"
 Dir["./solutions/*.rb"].each { |file| require file }
 
 class EulerTests < Minitest::Test
+  parallelize_me!
+
   def test_problem1
     assert_solution 23, Problem1.new(10)
     assert_solution 233168, Problem1.new(1000)
