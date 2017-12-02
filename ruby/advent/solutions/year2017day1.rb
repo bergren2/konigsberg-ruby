01d8bd783311e5ebeb0ecb81658fd21aa18aae1e
bergren2 <dan@heymrbass.com>
2017-12-02 09:30:08 -0600

    complete day 1 part 2

--- ruby/advent/solutions/year2017day1.rb
+++ ruby/advent/solutions/year2017day1.rb
@@ -1,19 +1,27 @@
-class Year2017Day1Part1
+class Year2017Day1
   include AdventSolvable
 
-  def initialize filename_or_int
+  def initialize filename_or_int, part
     if filename_or_int.end_with? ".txt"
       @n = File.open(resource_path(filename_or_int), &:readline).strip
     else # assumes int string
       @n = filename_or_int
     end
+
+    @part = part
   end
 
   def solution
     sum = 0
 
+    if @part == 1 then
+      jump = 1
+    else
+      jump = @n.size / 2
+    end
+
     @n.each_char.with_index do |c, i|
-      sum += c.to_i if c == @n[(i+1) % @n.size]
+      sum += c.to_i if c == @n[(i+jump) % @n.size]
     end
 
     sum
--- ruby/advent/tests.rb
+++ ruby/advent/tests.rb
@@ -12,10 +12,19 @@ class AdventTests < Minitest::Test
   parallelize_me!
 
   def test_year2017day1part1
-    assert_solution 3, Year2017Day1Part1.new("1122")
-    assert_solution 4, Year2017Day1Part1.new("1111")
-    assert_solution 0, Year2017Day1Part1.new("1234")
-    assert_solution 9, Year2017Day1Part1.new("91212129")
-    assert_solution 1089, Year2017Day1Part1.new("year2017day1part1.txt")
+    assert_solution 3, Year2017Day1.new("1122", 1)
+    assert_solution 4, Year2017Day1.new("1111", 1)
+    assert_solution 0, Year2017Day1.new("1234", 1)
+    assert_solution 9, Year2017Day1.new("91212129", 1)
+    assert_solution 1089, Year2017Day1.new("year2017day1.txt", 1)
+  end
+
+  def test_year2017day1part2
+    assert_solution 6, Year2017Day1.new("1212", 2)
+    assert_solution 0, Year2017Day1.new("1221", 2)
+    assert_solution 4, Year2017Day1.new("123425", 2)
+    assert_solution 12, Year2017Day1.new("123123", 2)
+    assert_solution 4, Year2017Day1.new("12131415", 2)
+    assert_solution 1156, Year2017Day1.new("year2017day1.txt", 2)
   end
 end
