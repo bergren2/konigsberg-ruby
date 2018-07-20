279963d4b876c3a411547cd3200780c560c798eb
bergren2 <dan@heymrbass.com>
2018-07-20 16:18:38 -0500

    load Coveralls earlier

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
+require "coveralls"
+Coveralls.wear!
+
 require "minitest/autorun"
 require "minitest/parallel"
 require "minitest/pride"
-require "coveralls"
-Coveralls.wear!
 
 require_relative "assertions"
