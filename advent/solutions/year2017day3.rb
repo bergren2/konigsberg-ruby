0926c683d9656c6f5afe4b5b32aaae2f020eaa47
bergren2 <dan@heymrbass.com>
2018-07-28 13:34:21 -0500

    cleanup

--- advent/solutions/year2017day3.rb
+++ advent/solutions/year2017day3.rb
@@ -4,7 +4,7 @@ require "cartesian"
 class Year2017Day3
   include AdventSolvable
 
-  def initialize input, part
+  def initialize(input, part)
     @input = input
     @part = part
     @cart = CartesianCoordinateSystem.new
@@ -16,11 +16,11 @@ class Year2017Day3
     @y = 0
 
     # seed
-    if @part == 1
-      @cart[0, 0] = 0
-    else # assume part 2
-      @cart[0, 0] = 1
-    end
+    @cart[0, 0] = if @part == 1
+                    0
+                  else # assume part 2
+                    1
+                  end
   end
 
   # 36  35  34  33  32  31
@@ -31,13 +31,13 @@ class Year2017Day3
   # 21  22  23  24  25  26
   def solution
     if @part == 1
-      (@input-1).times {
+      (@input - 1).times do
         # figure out value
         a = @cart.adjacent(@x, @y)
         @cart[@x, @y] = a.min + 1
 
         update_direction
-      }
+      end
     else # assume part 2
       while @cart.last <= @input
         a = @cart.surrounding(@x, @y)
@@ -54,28 +54,28 @@ class Year2017Day3
     # where to next?
     case @direction
     when :right
-      if @cart[@x, @y+1]
+      if @cart[@x, @y + 1]
         @x += 1
       else
         @direction = :up
         @y += 1
       end
     when :up
-      if @cart[@x-1, @y]
+      if @cart[@x - 1, @y]
         @y += 1
       else
         @direction = :left
         @x -= 1
       end
     when :left
-      if @cart[@x, @y-1]
+      if @cart[@x, @y - 1]
         @x -= 1
       else
         @direction = :down
         @y -= 1
       end
     when :down
-      if @cart[@x+1, @y]
+      if @cart[@x + 1, @y]
         @y -= 1
       else
         @direction = :right
