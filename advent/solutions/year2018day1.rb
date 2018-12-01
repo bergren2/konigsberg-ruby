a029d8c515d08b67a1f7e589537414ec1f94f09a
bergren2 <dan@heymrbass.com>
2018-12-01 02:44:10 -0600

    fix code style issues

--- advent/solutions/year2018day1.rb
+++ advent/solutions/year2018day1.rb
@@ -7,29 +7,29 @@ class Year2018Day1
     @part = part
 
     sequence_regex = /^([+-]\d+)(,\s+[+-]\d+)*$/
-    if sequence_regex.match? change_sequence_or_filename then
-      string_changes = change_sequence_or_filename.split(/,\s+/)
-    else
-      string_changes = File.open(resource_path(change_sequence_or_filename), &:readlines)
-    end
-    @changes = string_changes.map { |s| s.to_i }
+    string_changes = if sequence_regex.match? change_sequence_or_filename
+                       change_sequence_or_filename.split(/,\s+/)
+                     else
+                       File.open(resource_path(change_sequence_or_filename), &:readlines)
+                     end
+    @changes = string_changes.map(&:to_i)
   end
 
   def solution
-    if @part == 1 then
-      return @changes.sum
+    if @part == 1
+      @changes.sum
     else # part 2
       i = -1
       result = 0
       seen_once = Set.new
 
-      until seen_once.include?(result) do
+      until seen_once.include?(result)
         seen_once << result
         i += 1
         result += @changes[i % @changes.length]
       end
 
-      return result
+      result
     end
   end
 end
