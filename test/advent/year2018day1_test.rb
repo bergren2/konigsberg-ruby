1760989b76ebb7bea42133fcad48e9a2a483cca9
bergren2 <dan@heymrbass.com>
2018-12-01 03:05:12 -0600

    avoid ambiguous operator

--- test/advent/year2018day1_test.rb
+++ test/advent/year2018day1_test.rb
@@ -8,7 +8,7 @@ class Year2018Day1Test < Minitest::Test
     assert_solution 3, Year2018Day1.new("+1, -2, +3, +1", 1)
     assert_solution 3, Year2018Day1.new("+1, +1, +1", 1)
     assert_solution 0, Year2018Day1.new("+1, +1, -2", 1)
-    assert_solution -6, Year2018Day1.new("-1, -2, -3", 1)
+    assert_solution(-6, Year2018Day1.new("-1, -2, -3", 1))
     assert_solution 520, Year2018Day1.new("year2018day1.txt", 1)
   end
 
