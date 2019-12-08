46590c115874befbed1ea6c565ac1e21d412ef3f
bergren2 <bergren2@users.noreply.github.com>
2019-12-08 01:49:56 -0600

    cleanup legibility

--- advent/2019/year2019day2.rb
+++ advent/2019/year2019day2.rb
@@ -1,49 +1,49 @@
 require "advent"
 
 module Advent
-  class Year2019Day2
-    include Solvable
+  module Year2019Day2
+    class Part1
+      include Solvable
 
-    def initialize(filename_or_intcode, part)
-      @part = part
+      def initialize(filename_or_intcode)
+        intcodes = if filename_or_intcode.end_with? ".txt"
+                     codes = File.open(resource_path(filename_or_intcode), &:readline).split(",")
 
-      intcodes = if filename_or_intcode.end_with? ".txt"
-                   codes = File.open(resource_path(filename_or_intcode), &:readline).split(",")
-
-                   # create 1202 program alarm
-                   codes[1] = 12
-                   codes[2] = 2
-                   codes
-                 else # assume Intcode
-                   filename_or_intcode.split(",")
-                 end
-      @program = intcodes.map(&:to_i)
-    end
-
-    def solution
-      head = 0
-
-      while @program[head] != 99
-        op = @program[head]
-        n1 = @program[head + 1]
-        n2 = @program[head + 2]
-        out = @program[head + 3]
-
-        # complete operation
-        case op
-        when 1 # add
-          @program[out] = @program[n1] + @program[n2]
-        when 2 # multiply
-          @program[out] = @program[n1] * @program[n2]
-        else
-          throw "Don't understand op '#{op}'"
-        end
-
-        # move head to next location
-        head += 4
+                     # create 1202 program alarm
+                     codes[1] = 12
+                     codes[2] = 2
+                     codes
+                   else # assume Intcode
+                     filename_or_intcode.split(",")
+                   end
+        @program = intcodes.map(&:to_i)
       end
 
-      @program[0]
+      def solution
+        head = 0
+
+        while @program[head] != 99
+          op = @program[head]
+          n1 = @program[head + 1]
+          n2 = @program[head + 2]
+          out = @program[head + 3]
+
+          # complete operation
+          case op
+          when 1 # add
+            @program[out] = @program[n1] + @program[n2]
+          when 2 # multiply
+            @program[out] = @program[n1] * @program[n2]
+          else
+            throw "Don't understand op '#{op}'"
+          end
+
+          # move head to next location
+          head += 4
+        end
+
+        @program[0]
+      end
     end
   end
 end
